9899ec081918ee184ba65ab24fecbfd4444d2c57
hhjo <kikiki0611@naver.com>
2018-12-17 09:34:47 +0900

    Fix to cannot load chrome driver at Linux

--- WebDriverFactory.py
+++ WebDriverFactory.py
@@ -7,12 +7,12 @@ class WebDriverFactory:
 
     def createChromeDriver(self):
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument('--window-size=1420,1080')
-        # chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
 
-        driver = webdriver.Chrome(self._driverPath, chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path=self._driverPath, chrome_options=chrome_options)
         driver.implicitly_wait(3)
 
         return driver
